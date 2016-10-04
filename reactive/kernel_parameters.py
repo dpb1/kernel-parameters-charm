5ad99e1c69c87c8fcd4c578602db3a2cc1aa54d9
David Britton <davidpbritton@gmail.com>
2016-10-03 20:04:42 -0600

    shuffle tests around, tox now passing.

--- reactive/kernel_parameters.py
+++ reactive/kernel_parameters.py
@@ -1,7 +1,8 @@
+import os
+import shutil
+import subprocess
 from charms.reactive import when_not, set_state
 from charmhelpers.core import hookenv
-
-from lib.helpers import write_sysctl, load_sysctl_file
 
 SYSCTL_FILE = "/etc/sysctl.d/90-kernel-parameters-charm"
 
@@ -15,3 +16,17 @@ def install_kernel_parameters():
     write_sysctl(params, SYSCTL_FILE)
     load_sysctl_file(SYSCTL_FILE)
     set_state('kernel-parameters.installed')
+
+
+def write_sysctl(params, path):
+    """Write sysctl file from params, unlink first if exists."""
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    with open(path, 'w') as f:
+        for param in params.split(";"):
+            f.write("%s\n" % param.strip())
+
+
+def load_sysctl_file(path):
+    """Load the given sysctl file."""
+    subprocess.check(["sysctl", "-p", path])
